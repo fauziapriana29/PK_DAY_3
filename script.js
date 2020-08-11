d528bbfe61c5815ee3fca9064cac3ccd8a02ff96
fauziapriana29 <fauzicoding@gmail.com>
2020-08-11 16:00:10 +0700

    latihan ke_3

---
+++ script.js
@@ -0,0 +1,7 @@
+window.alert("hello world!!");
+window.prompt("Masukan Nama Kalian ?")
+document.getElementById('element');
+console.log(document.getElementById('element'));
+
+document.body.innerHTML += "<h1>hello world</h1>"
+document.getElementById('element').innerHTML = '<h3>Hello</h3>'
